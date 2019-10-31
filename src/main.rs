9a574629ba5c76236004899dcefbc7ec1d1aa918
fran <fran.sl@atlascity.io>
2019-10-31 20:15:27 +0000

    blinky working

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
-//! Prints "Hello, world!" on the host console using semihosting
-
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
-extern crate panic_halt;
+// Halt on panic
+#[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
+extern crate panic_halt; // panic handler
 
+use cortex_m;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{debug, hprintln};
+use stm32f4xx_hal as hal;
+
+use crate::hal::{prelude::*, stm32};
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
-    //debug::exit(debug::EXIT_SUCCESS);
+    if let (Some(dp), Some(cp)) = (
+        stm32::Peripherals::take(),
+        cortex_m::peripheral::Peripherals::take(),
+    ) {
+        // Set up the LED. On the Nucleo-446RE it's connected to pin PA5.
+        let gpioa = dp.GPIOA.split();
+        let mut led = gpioa.pa5.into_push_pull_output();
+
+        // Set up the system clock. We want to run at 48MHz for this one.
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
+
+        // Create a delay abstraction based on SysTick
+        let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
+
+        loop {
+            // On for 1s, off for 1s.
+            led.set_high().unwrap();
+            delay.delay_ms(1000_u32);
+            led.set_low().unwrap();
+            delay.delay_ms(1000_u32);
+        }
+    }
+
     loop {}
 }
